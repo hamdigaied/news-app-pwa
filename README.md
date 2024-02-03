00562561c658126974ec664894345fe981ab67a2
Hamdi Gaied <hamdi.gaied.si@gmail.com>
2024-02-03 05:31:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# News App PWA
+
+News app built using HTML, CSS and Javascript.
+A layer of PWA will be added later.
---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const newsContainer = document.getElementById('news-container');
+
+    fetch('articles.json')
+        .then(response => response.json())
+        .then(articles => {
+            // Display each news article in the container
+            articles.forEach(article => {
+                const newsItem = document.createElement('div');
+                newsItem.classList.add('news-item');
+                newsItem.innerHTML = `
+                        <h2>${article.title}</h2>
+                        <img src="${article.urlToImage}" alt="Article Image">
+                        <p>${article.description}</p>
+                        <div class="news-details">
+                            <span>Published at: ${new Date(article.publishedAt).toLocaleString()}</span>
+                            <span>Author: ${article.author || 'Unknown'}</span>
+                            <a href="${article.url}" target="_blank">Read more</a>
+                        </div>
+                    `;
+                newsContainer.appendChild(newsItem);
+            });
+        })
+        .catch(error => newsContainer.innerHTML = '<h3>No articles</h2>');
+});
